be72cde90b52c61aac5208de9c3220143477e0da
amit9415 <amit9415@gmail.com>
2024-10-14 15:22:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,7 @@ print(VARIABLE_1)
 print("ENVIROMENT_1***")
 ENVIROMENT_1 = os.environ['ENVIROMENT_1']
 print(ENVIROMENT_1)
+
+print("ENVIRONMENT_NAME** from github env***")
+ENVIRONMENT_NAME = os.environ['ENVIRONMENT_NAME']
+print(ENVIRONMENT_NAME)
